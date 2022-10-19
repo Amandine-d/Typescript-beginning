d4623a94ede985092911e8c7437895b9d0d50dfe
Amandine <amandine.druart@gmail.com>
2022-10-19 13:31:31 +0200

    Create ToDos component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import Todos from "./components/Todos";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Todos />
     </div>
   );
 }
 
 export default App;
+
