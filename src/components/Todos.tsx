5cb7be61910daac670679f4d6c16d119807462b9
Amandine <amandine.druart@gmail.com>
2022-10-19 13:45:47 +0200

    Todos: add proper typescript annotation

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,19 @@
-function Todos() {
+// function Todos(props) {
+//props: { items: string[]} => props is an object, with the items key, which itself holds an array of strings
+const Todos: React.FC<{ items: string[] }> = (props) => {
+  //<> React.FC is already a generic type
+  //we are using a generi type here, before we defined it
+
+  //makes it clear that this acts as a Functional Component
+  //will allow props to receive a children property
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn React</li>
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
+
