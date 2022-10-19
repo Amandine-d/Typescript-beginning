807c6930c70af0d55671f8f5e958d6369f9e60be
Amandine <amandine.druart@gmail.com>
2022-10-19 14:52:16 +0200

    Todo: manage state and ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn Ts")];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  //Pb: pass an emty array as a default value, but ts can't make out what it is: an array of todos. We have tot ell ts with <>
 
   const addTodoHandler = (todoText: string) => {
-    
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
   };
 
   return (
